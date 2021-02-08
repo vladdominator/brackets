5b5209f2b97efefbf698b9cba2f6f191b83fab96
vladdominator <73780858+vladdominator@users.noreply.github.com>
2021-02-08 18:12:07 +0300

    feat: add two arrays

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let one = [];
+   let two = [];
+     for (let i = 0; i < bracketsConfig.length; i++) {
+         one.push(bracketsConfig[i][0]);
+         two.push(bracketsConfig[i][1]);
+     }
+     let stack = [];
 }
